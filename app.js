60f680fde04fc68e155ae788340f535162ef688b
BuildTools <unconfigured@null.spigotmc.org>
2019-02-11 22:42:40 +0900

    ポートスキャン思案

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-// const NetcatServer = require('netcat/server')
+const NetcatServer = require('netcat/server')
 const NetcatClient = require('netcat/client')
 // var express = require('express');
-// var nc = require('node-netcat');
-var nc = new NetcatClient()
+var nc = require('node-netcat');
+// var nc = new NetcatClient()
 // var app = express();
 // var exec = require('child_process').exec;
 // var util = require('util');
 // var http = require('http').Server(app);
 // var fs = require('fs');
-// const nmap = require('libnmap');
+const nmap = require('libnmap');
+var lsof = require('lsof');
+var portscanner = require('portscanner')
 // const io = require('socket.io')(http);
 // const PORT = process.env.Port || 3000;
-setInterval(function() {
+// setInterval(function() {
 
+// var port = '4001';
 
-nc.addr('192.168.0.3').scan('25565-25566', function(ports){
-  // ports: { '22': 'open', '23': 'closed' ... }
-  console.log(ports['25565']);
- })
-}, 1000);
+// nc.addr('192.168.0.3').scan(port+'-'+port, function(ports){
+//   // ports: { '22': 'open', '23': 'closed' ... }
+//   console.log(ports[port]);
+//  })
+// }, 500);
 
 
 // nc.udp().port(9987).wait(1000).init().send('hello', '192.168.0.3')
@@ -100,3 +103,52 @@ nc.addr('192.168.0.3').scan('25565-25566', function(ports){
 //   console.log('server listening. Port:' + PORT);
 
 // });
+
+// const opts = {
+//   range : ['192.168.0.3'],
+//   // range : ['192.168.0.3','192.168.0.3'],
+
+//   ports : '4000'
+// };
+
+// nmap.scan(opts, function(err, report) {
+//   if (err) throw new Error(err);
+//   // console.log("open");
+//   for (let item in report) {
+//     // console.log(report[item]['host']['item']);
+//     console.log(JSON.stringify(report[item]['host']));
+//     // console.log(JSON.stringify(report[item]));
+//     // console.log(item);
+
+//   }
+// });
+
+// portscanner.checkPortStatus(4001, '192.168.0.3', function(error, status) {
+//   // Status is 'open' if currently in use or 'closed' if available
+//   console.log(status)
+// })
+
+// nc.udp().port(9987).wait(1000).init().send('hello', '192.168.0.3')
+
+// nc.addr('192.168.0.3').scan('4000-4001', function(ports){
+//   // ports: { '22': 'open', '23': 'closed' ... }
+//   console.log(ports);
+
+//  })
+// nc.client('25568', '192.168.0.3');
+// var client = nc.udpClient('4001', '192.168.0.3');
+
+// client.on('open', function () {  console.log('open'); });
+
+// client.once('error', function (err) {  console.error('err'); });
+
+// client.once('close', function () { console.log('client, closed'); });
+
+// client.send('Hello World');
+// new nc.udpClient('4001', '192.168.0.3');
+
+var scan = nc.portscan();
+
+scan.run('192.168.0.3', '4000-4001', function (err, res) {
+	console.log(err, res);
+});
