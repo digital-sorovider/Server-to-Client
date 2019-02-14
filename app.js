e2a1c8c2fdf2dab6569d5f0416f3ad9c0693a1b0
BuildTools <unconfigured@null.spigotmc.org>
2019-02-14 13:47:08 +0900

    サーバーステータス変化取得

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var app = express();
 var exec = require('child_process').exec; //シェルコマンド実行モジュール
 const is_windows = process.platform === 'win32'
 const is_linux = process.platform === 'linux'
-const is_mac = process.platform === 'darwin'
 // var util = require('util');
 var http = require('http').Server(app);
 // var fs = require('fs');
@@ -14,23 +13,23 @@ const PORT = process.env.Port || 3000;
 
 var status;
 
-var check_port = "4000"
-	port = '":' + check_port + ' "'
+check_port = "4000"
+port = '":' + check_port + ' "'
 
-function listen() {
+function listen_check() {
 	// var search_type;
 
+	//実行OSを基にnetstatとコマンドのオプションと検索コマンドの分岐
 	if (is_windows) {
 		args = ' -anp UDP '
 		search_type = ' find '
 	}
-	else if (is_linux) {
+
+	if (is_linux) {
 		args = ' -anu '
 		search_type = ' grep '
 	}
-	else {
-		search_type = '?'
-	}
+
 
 	//指定されたポートがlistenされているかどうか判定
 	var result = new Promise(function (resolve) {
@@ -43,41 +42,59 @@ function listen() {
 	//（上記の判定が終了した後）前回の判定と今回の判定結果が違う場合はサーバーのステータスが変化したことをクライアントに知らせる
 	result.then(function (data) {
 		// io.emit('message_s', true);
+		// if (data) {
+		// 	io.emit('server_status', "Running!!");
+		// }
+		// else{
+		// 	io.emit('server_status', "Not Run!");
 
-		if (status !== data){
+		// }
+
+		if (status !== data) {
 			// console.log(data)
-		if(data){
-			io.emit('message_s', "サーバーの起動が完了しました");
-			io.emit('server_status', "Running!!");
-		}
-		else {
-			io.emit('message_s', "サーバーの停止が完了しました");
-			io.emit('server_status', "Not Run!!");
 
-		}
-		status = data
+			if (data) {
+				io.emit('message_s', "サーバーが稼働状態になりました");
+				io.emit('server_status', "Running!!");
+
+			}
+			else {
+				io.emit('message_s', "サーバーが停止状態になりました");
+				io.emit('server_status', "Not Run!");
+
+			}
+			status = data
 		}
 	});
+
 
 }
 
 //1秒ごとに判定
-setInterval(listen, 1000)
+// setInterval(listen_check, 1000)
 
 io.on('connection', function (socket) {
-//   var text = fs.readFileSync("test.txt").toString();
-//   io.emit('message_s', text);
-	listen()
-  socket.on('message', function (msg) {
-    io.emit('message_s', msg);
-    io.emit('message_s', "unti");
-    // io.emit('message_s', util.inspect(ls));
+	// listen_check()
+	console.log("connect");
 
-  });
+	if (status) {
+		io.emit('server_status', "Running!!");
+	}
+	else {
+		io.emit('server_status', "Not Run!");
+	}
+
+	//   var text = fs.readFileSync("test.txt").toString();
+	//   io.emit('message_s', text);
+	socket.on('message', function (msg) {
+		io.emit('message_s', msg);
+		// io.emit('message_s', util.inspect(ls));
+
+	});
 });
 
 http.listen(PORT, function () {
-  console.log('server listening. Port:' + PORT);
+	console.log('server listening. Port:' + PORT);
 
 });
 
